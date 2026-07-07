edddf2a2fcd1755e5796cb7390b3cdd119619c69
luvila9 <baydri04@gmail.com>
2026-07-08 03:56:16 +0700

    Fix API: Mengaktifkan mode server di Vercel secara dinamis

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  // Teknik Bunglon: 
+  // Jika Vercel (ada env VERCEL), jangan gunakan output export.
+  // Jika lokal/build biasa, tetap gunakan output export untuk Android.
+  output: process.env.VERCEL ? undefined : 'export',
+  
   images: {
     unoptimized: true,
   },
